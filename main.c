b4b5abb7a45933095576e46777ab46ec17e559d6
Finn Barber <finnbarber@protonmail.com>
2021-07-25 11:13:06 +0100

    Removed curl write function, since it uses the same function by default

--- main.c
+++ main.c
@@ -5,22 +5,13 @@
 
 CURL* curl_handle;
 
-static size_t curl_write_fn(void *start, size_t sz, size_t bytes, void *file)
-{
-  // Used by curl to write to a file.
-  return fwrite(start, sz, bytes, file);
-}
-
 void url_to_file(char* url, char* filename)
 {
   // Writes data from a URL to a file.
   FILE* out_file = fopen(filename, "wb");
   if (!out_file) { fprintf(stderr, "Cannot load URL to file!\n"); exit(EXIT_FAILURE); }
-  curl_easy_setopt(curl_handle, CURLOPT_URL, url);                     // Set the URL
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);               // Disable the progress bar
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, curl_write_fn); // Use my write function
-  curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L);           // Follow redirects
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, out_file);          // Set output file
+  curl_easy_setopt(curl_handle, CURLOPT_URL, url);            // Set the URL
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, out_file); // Set output file
   curl_easy_perform(curl_handle);
   fclose(out_file);
 }
@@ -29,6 +20,9 @@ int main()
 {
   curl_global_init(CURL_GLOBAL_ALL);
   curl_handle = curl_easy_init();
-  url_to_file("www.wikipedia.com", "wikipedia.out");
+  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1");
+  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);     // Disable the progress bar
+  curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects
+  url_to_file("upload.wikimedia.org/wikipedia/commons/6/68/Barnard_33.jpg", "horsehead.jpg");
   curl_easy_cleanup(curl_handle);
 }
