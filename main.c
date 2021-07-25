7327baa824e1362db491362605ee7617f3772ce9
Finn Barber <finnbarber@protonmail.com>
2021-07-25 11:34:35 +0100

    Print a fancy error message if anything goes wrong

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <execinfo.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <signal.h>
 #include <unistd.h>
 #include <curl/curl.h>
+#include "main.h"
 
 CURL* curl_handle;
 
@@ -9,7 +16,7 @@ void url_to_file(char* url, char* filename)
 {
   // Writes data from a URL to a file.
   FILE* out_file = fopen(filename, "wb");
-  if (!out_file) { fprintf(stderr, "Cannot load URL to file!\n"); exit(EXIT_FAILURE); }
+  if (!out_file) throw_error("Cannot load URL %s to file %s", url, filename);
   curl_easy_setopt(curl_handle, CURLOPT_URL, url);            // Set the URL
   curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, out_file); // Set output file
   curl_easy_perform(curl_handle);
@@ -18,11 +25,42 @@ void url_to_file(char* url, char* filename)
 
 int main()
 {
+  // Bind error signal handlinjg functions.
+  char signals[] = { SIGHUP, SIGINT, SIGQUIT, SIGILL, SIGABRT, SIGBUS, SIGFPE, SIGSEGV, SIGPIPE, SIGTERM, SIGCHLD };
+  for (size_t i = 0; i < sizeof(signals); ++i) signal(signals[i], handle_error_signal);
+  // Init libcURL
   curl_global_init(CURL_GLOBAL_ALL);
   curl_handle = curl_easy_init();
   curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1");
   curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);     // Disable the progress bar
   curl_easy_setopt(curl_handle, CURLOPT_FOLLOWLOCATION, 1L); // Follow redirects
-  url_to_file("upload.wikimedia.org/wikipedia/commons/6/68/Barnard_33.jpg", "horsehead.jpg");
+  // Actual code
+  url_to_file("www.wikipedia.com", "wikipedia.out");
+  // Cleanup
   curl_easy_cleanup(curl_handle);
+}
+
+__attribute__((format(printf, 1, 2))) void throw_error(char* fmt, ...)
+{
+  fputs("\033[31;1m", stderr);
+  va_list args;
+  va_start(args, fmt);
+  vfprintf(stderr, fmt, args);
+  fputc('\n', stderr);
+  if (errno)
+  {
+    const char* str = strerror(errno);
+    fprintf(stderr, "\033[0m(%c%s)\n", tolower(*str), str + 1);
+  }
+  fputs("\033[0;2m", stderr);
+  void *trace_array[10];
+  puts("begin backtrace...");
+  size_t size = backtrace(trace_array, 10);
+  backtrace_symbols_fd(trace_array, size, STDERR_FILENO);
+  exit(EXIT_FAILURE);
+}
+
+_Noreturn void handle_error_signal(int sig)
+{
+  throw_error("Recieved signal %i (%s)", sig, strsignal(sig));
 }
---
+++ main.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void url_to_file(char*, char*);
+_Noreturn void throw_error(char*, ...);
+_Noreturn void handle_error_signal(int);
